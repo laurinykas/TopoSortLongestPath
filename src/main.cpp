69942b0bd13132128318b4d6f8ef398faca808ba
laurinykas <112711939+laurinykas@users.noreply.github.com>
2023-12-29 20:20:50 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+
+#include "../Include/funkcijos.h"
+ vector <Studentai> stud;
+ vector<Studentai> kieti;
+ vector<Studentai> vargsai;
+Studentai duomenys;
+ int studentu_kiekis = 0;
+
+
+int main() {
+    
+    Fake_main();
+    system("pause");
+}
+
+
+
+
+
